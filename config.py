421ee6e49833b16c1bee195ddf84e0c87a6e5514
Varshith <varsh@MacBookAir.vodafone.ultrahub>
2026-04-13 23:27:59 +0100

    Adding more queries and increasing results_per_query

--- config.py
+++ config.py
@@ -12,5 +12,13 @@ YOUTUBE_API_VERSION = "v3"
 DB_PATH = "youtube_data.db"
 TABLE_NAME = "videos"
 
-VIRAL_QUERIES = ["music"]
-RESULTS_PER_QUERY = 10
+VIRAL_QUERIES = ["most viral videos 2025",
+    "funny viral moments 2025",
+    "viral life hacks 2025",
+    "viral music 2025",
+    "viral challenge 2025",
+    "viral news 2025",
+    "viral tech review 2025",
+    "viral food recipe 2025",
+]
+RESULTS_PER_QUERY = 30
